10d95f18502d88bcf33bdda10941cf2c4655077b
André Gross <andgross@gmail.com>
2022-07-28 18:21:13 -0300

    Requisito #1 Ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// feito com ajuda do Sérgio Henrique e João Marcelo e Thalis Venancio
+function getSpeciesByIds(...ids) {
+  return data.species.filter((element) => ids.find((parm1) => parm1 === element.id));
 }
 
 module.exports = getSpeciesByIds;
