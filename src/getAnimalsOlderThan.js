56263115c0debb394e1092cff16fc8967f54883b
André Gross <andgross@gmail.com>
2022-07-29 15:03:31 -0300

    Requisito #3 Ok

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+// Implemente a função getAnimalsOlderThan que deve receber uma espécie e uma idade como parâmetro,
+
+// retornar se todos os animais dessa espécie possuem essa idade ou são mais velhos.
+
+// Verifique se todos os animais da espécie passada como parâmetro possuem a idade mínima:
+
+// Os animais devem ter essa idade ou serem mais velhos.
+
+// Retorne um valor booleano.
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species
+    .find((element) => element.name === animal).residents
+    .every((morador) => morador.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,10 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage(object) {
-  const parcialResult = data.employees.find((element) => element.firstName === object.name
+function getEmployeesCoverage() {
+/*   const parcialResult = data.employees.find((element) => element.firstName === object.name
     || element.lastName === object.name || element.id === object.id);
+  return parcialResult; */
 }
-
-console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
