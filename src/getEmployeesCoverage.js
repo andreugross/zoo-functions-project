0bc55c94130910801050011d57aafa4a2078f697
André Gross <andgross@gmail.com>
2022-08-02 16:18:44 -0300

    Requisito #11 Ok

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,9 +1,40 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-/*   const parcialResult = data.employees.find((element) => element.firstName === object.name
-    || element.lastName === object.name || element.id === object.id);
-  return parcialResult; */
+const achaColaborador = (object) => {
+  let parcialResult;
+  if (!object) {
+    return data.employees.map((element) => element);
+  }
+  if (object) {
+    parcialResult = data.employees.find((element) => element.firstName === object.name
+      || element.lastName === object.name || element.id === object.id);
+  }
+  if (!parcialResult) {
+    throw new Error('Informações inválidas');
+  }
+  return parcialResult;
+};
+
+const achaNomeAnimal = (object) => object.responsibleFor.map((animalId) =>
+  data.species.find((specie) => specie.id === animalId).name);
+
+const achaLocalAnimal = (object) => object.responsibleFor.map((animalId) =>
+  data.species.find((specie) => specie.id === animalId).location);
+
+function getEmployeesCoverage(object) {
+  if (!object) {
+    return achaColaborador().map((element) => ({
+      id: element.id,
+      fullName: `${element.firstName} ${element.lastName}`,
+      species: achaNomeAnimal(element),
+      locations: achaLocalAnimal(element),
+    }));
+  } return {
+    id: achaColaborador(object).id,
+    fullName: `${achaColaborador(object).firstName} ${achaColaborador(object).lastName}`,
+    species: achaNomeAnimal(achaColaborador(object)),
+    locations: achaLocalAnimal(achaColaborador(object)),
+  };
 }
 
 module.exports = getEmployeesCoverage;
